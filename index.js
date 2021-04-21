f31453404cfcc1313cb78922bca6bb4bfb9f9e5e
fitzgebt <fitzgebt@gmail.com>
2021-04-21 10:54:10 -0700

    commit tree e4dcc3fb2918f45c6bced9a0f36588930f173964

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4
+let max = Math.max(5,10,15,20)
+let random = Math.floor(Math.random() * 1000) + 1
+let multiply = num1 * num2
